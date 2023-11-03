71daae4e1762a690226d61ae1399fedf7502adda
papondreo <123875656+papondreo@users.noreply.github.com>
2023-11-03 18:47:51 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
